2957f00ea017bed3bf8b20ee39a8de5ffa5a6c9d
st10066248 <128744069+st10066248@users.noreply.github.com>
2024-09-10 02:10:52 +0200

    Add files via upload
    Changes to the Home Page design were made along with the Lecturer Home Page

---
+++ LecturerPortal.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Prog6212
+{
+    /// <summary>
+    /// Interaction logic for LecturerPortal.xaml
+    /// </summary>
+    public partial class LecturerPortal : Window
+    {
+        public LecturerPortal()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
